85afe44f2c93be2d1a60f482e5276000d877da12
Massimiliano Burzi <massi@MacBook-Pro.local>
2023-11-25 18:23:27 +0100

    first commit

---
+++ F1_Project/test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+driversData = pd.read_csv('data/F1DriversDataset.csv')
+#print(driversData)
+plt.bar(driversData["Driver"][1:5], height = driversData["Years_Active"][1:5])
+plt.show()
+
+
+#driversData.head()
