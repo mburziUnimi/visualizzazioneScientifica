63b1774e1f5de1388b5271b110d3fc1a3c8a7f9a
Laura Monti <l_monti@fastwebnet.it>
2023-11-25 18:42:41 +0100

    prova di commit

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ driversData = pd.read_csv('data/F1DriversDataset.csv')
 plt.bar(driversData["Driver"][1:5], height = driversData["Years_Active"][1:5])
 plt.show()
 
-
+#commento di prova
 #driversData.head()
