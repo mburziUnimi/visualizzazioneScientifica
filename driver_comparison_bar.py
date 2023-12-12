ef0beb7f595923736946822c0cf186ff751c1f75
Laura Monti <l_monti@fastwebnet.it>
2023-12-12 19:27:04 +0100

    dati confronti piloti

--- driver_comparison_bar.py
+++ driver_comparison_bar.py
@@ -19,22 +19,18 @@ posgained = [sommaD1,sommaD2]
 
 #driversData.head()
 
-#per aggiungere i label sulle barre
-def addlabels(x,y):
-    for i in range(len(x)):
-        plt.text(i, y[i], y[i], ha = 'center')
+
 
 fig, ax = plt.subplots()
-ax.barh([D1, D2], posgained, 0.4)
+bars = ax.barh([D1, D2], posgained, 0.5)
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.spines['bottom'].set_visible(True)
 ax.spines['left'].set_visible(True)
 #ax.invert_yaxis()   # labels read top-to-bottom
+ax.bar_label(bars, padding=10)
 ax.set_xlabel('Posizioni')
-addlabels(posgained, posgained)
 plt.title('Posizioni conquistate nella stagione')
 
 plt.show()
 
-# %%
